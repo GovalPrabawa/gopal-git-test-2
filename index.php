f1bee43200169fa72ff2c41ac3ee0099c8612649
Goval Prabawa <imadegoval726@gmail.com>
2022-10-09 11:38:23 +0800

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remot</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ad sint accusamus soluta nihil. Ad minus eligendi tempora illum?</p>
+</body>
+</html>
